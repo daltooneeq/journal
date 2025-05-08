894320e955a5c522b00ecbaf2ebee15a508c3367
daltooneeq <danilbakurov1702@gmail.com>
2025-05-08 21:27:17 +0300

    gitignore

---
+++ release/journal-installer.sh
@@ -0,0 +1,6 @@
+
+sudo mv journal /usr/local/bin/
+
+touch ~/.jornaldir.txt
+
+echo "journal installed succefully. Write 'journal dir' for choose your journal's folder. Write 'journal help' for more information."
